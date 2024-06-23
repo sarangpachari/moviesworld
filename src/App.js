7c318097078532b0ee000e6698a9147c4642be8d
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-06-23 16:09:44 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=40e0e479'
+const API_URL = 'https://www.omdbapi.com/?apikey=40e0e479'
 
 
 
